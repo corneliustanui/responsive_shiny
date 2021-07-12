ba586e03bed4b47a4bd5766bd9ea50ea41ad66de
corneliustanui <kiplimocornelius@gmail.com>
2021-07-12 16:33:57 +0300

    Initial

---
+++ git config.R
@@ -0,0 +1,10 @@
+# Check Git Configurations (ensure you are able to push from origin)
+
+gh::gh_whoami() # to see git settings
+usethis::git_sitrep() # more settings
+
+usethis::create_github_token() # to create a token
+gitcreds::gitcreds_set() # to save the token 
+
+gh::gh_whoami() # to see git settings
+usethis::git_sitrep() # more settings
