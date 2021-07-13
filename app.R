49c223e6082dfc97f70da8be35fc3fcdc5bd0821
corneliustanui <kiplimocornelius@gmail.com>
2021-07-13 17:56:50 +0300

    COmmit 3

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
+# load shiny package 
 library(shiny)
 
+# load user interface
 source("ui.R")
 
+# load server
 source("server.R")
 
+# combine ui and server
 shinyApp(ui, server)
-
-
 
--- ui.R
+++ ui.R
@@ -1,5 +1,7 @@
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  selectInput("dataset", 
+              label = "Dataset", 
+              choices = ls("package:datasets")),
   verbatimTextOutput("summary"),
   tableOutput("table")
 )
